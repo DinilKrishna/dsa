de760c32cf66094711c4b52f874d5bca15319d32
dinil <dinilkrishna46@gmail.com>
2024-01-10 18:52:16 +0530

    insertion problems

--- insertionsort.py
+++ insertionsort.py
@@ -19,7 +19,7 @@ def insertion_sort_len(a):
     n = len(a)
     for i in range(1, n):
         for j in range(i, 0, -1):
-            if a[j] < a[j-1]:
+            if len(a[j]) < len(a[j-1]):
                 a[j], a[j-1] = a[j-1], a[j]                
 
 arr = [3,63,1,6,24,6,7]
@@ -29,5 +29,5 @@ insertion_sort(arr)
 print('sorted array: ',arr)
 insertion_sort_desc(arr)
 print('desc sorted array: ',arr)
-insertion_sort_len(arr)
-print('len sorted array: ',arr)
+insertion_sort_len(names)
+print('len sorted array: ',names)
