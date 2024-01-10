06ac24470ea4bdcc071ff3726d8babe3059d5b08
dinil <dinilkrishna46@gmail.com>
2024-01-10 18:50:41 +0530

    bubble problems

--- bubblesort.py
+++ bubblesort.py
@@ -7,7 +7,33 @@ def bubble_sort(a):
                 a[j], a[j+1] = a[j+1], a[j]
 
 
+def bubble_sort_desc(a):
+    n = len(a)
+    for i in range(n):
+        for j in range(n-i-1):
+            if a[j] < a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+
+def bubble_sort_len(a):
+    n = len(a)
+    for i in range(n):
+        for j in range(n-i-1):
+            if len(a[j]) > len(a[j+1]):
+                a[j], a[j+1] = a[j+1], a[j]
+
+
+
 a = [1,4,6,3,8,4,7,9,3]
 
 bubble_sort(a)
 print(a)
+
+b = [6.2345, 2.2546, 2.2545, 34.34536, 73.34534, 1.3633, 3.353563]
+
+bubble_sort_desc(b)
+print(b)
+
+c = ["dinil" , "saju", "arun", "anyanya", "rahul", "ashish"]
+
+bubble_sort_len(c)
+print(c)
--- insertionsort.py
+++ insertionsort.py
@@ -6,8 +6,28 @@ def insertion_sort(a):
             if a[j] < a[j-1]:
                 a[j], a[j-1] = a[j-1], a[j]
 
+
+def insertion_sort_desc(a):
+    n = len(a)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if a[j] > a[j-1]:
+                a[j], a[j-1] = a[j-1], a[j]
+
+
+def insertion_sort_len(a):
+    n = len(a)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if a[j] < a[j-1]:
+                a[j], a[j-1] = a[j-1], a[j]                
+
 arr = [3,63,1,6,24,6,7]
+names = ["dinil" , "saju", "arun", "anyanya", "rahul", "ashish"]
 
 insertion_sort(arr)
-
-print(arr)
+print('sorted array: ',arr)
+insertion_sort_desc(arr)
+print('desc sorted array: ',arr)
+insertion_sort_len(arr)
+print('len sorted array: ',arr)
