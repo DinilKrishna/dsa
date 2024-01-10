36badca51c2cdbb89ae8cbfe8eb931c6c1ec3e48
dinil <dinilkrishna46@gmail.com>
2024-01-10 19:30:38 +0530

    mergesort problems

--- mergesort.py
+++ mergesort.py
@@ -26,7 +26,69 @@ def merge_sort(arr):
             j += 1
             k += 1
 
+def merge_sort_desc(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort_desc(left)
+        merge_sort_desc(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] > right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+def merge_sort_len(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge_sort_len(left)
+        merge_sort_len(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if len(left[i]) < len(right[j]):
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+
+
 arr = [1,5,7,4,2,4,7,-2,9,56,-3,6]
 
 merge_sort(arr)
 print(arr)
+merge_sort_desc(arr)
+print(arr)
+
+names = ["dinil" , "saju", "arun", "anyanya", "rahul", "ashish"]
+merge_sort_len(names)
+print(names)
