375e268754e63de6dc3788399903ff05f7411a13
dinil <dinilkrishna46@gmail.com>
2024-01-15 10:39:37 +0530

    merge linked lists

--- insertionsort.py
+++ insertionsort.py
@@ -2,25 +2,37 @@
 def insertion_sort(a):
     n = len(a)
     for i in range(1, n):
-        for j in range(i, 0, -1):
-            if a[j] < a[j-1]:
-                a[j], a[j-1] = a[j-1], a[j]
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+
+        a[j + 1] = key
 
 
 def insertion_sort_desc(a):
     n = len(a)
     for i in range(1, n):
-        for j in range(i, 0, -1):
-            if a[j] > a[j-1]:
-                a[j], a[j-1] = a[j-1], a[j]
+        key = a[i]
+        j = i - 1
+        while j >= 0 and key > a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+
+        a[j + 1] = key
 
 
 def insertion_sort_len(a):
     n = len(a)
     for i in range(1, n):
-        for j in range(i, 0, -1):
-            if len(a[j]) < len(a[j-1]):
-                a[j], a[j-1] = a[j-1], a[j]                
+        key = a[i]
+        j = i - 1
+        while j >= 0 and len(key) < len(a[j]):
+            a[j + 1] = a[j]
+            j -= 1
+
+        a[j + 1] = key        
 
 arr = [3,63,1,6,24,6,7]
 names = ["dinil" , "saju", "arun", "anyanya", "rahul", "ashish"]
--- linkedlist.py
+++ linkedlist.py
@@ -1,32 +1,74 @@
-# class Node:
-#     def __init__(self, data):
-#         self.data = data
-#         self.ref = None
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
 
-# class LinkedList:
-#     def __init__(self):
-#         self.head = None
+class LinkedList:
+    def __init__(self):
+        self.head = None
 
-#     def print_LL(self):
-#         if self.head is None:
-#             print("Linked List is empty")
-#         else:
-#             n = self.head
-#             while n is not None:
-#                 print(n.data)
-#                 n = n.ref
+    def print_LL(self):
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n = n.ref
 
-#     def add_begin(self, data):
-#         new_node = Node(data)
-#         new_node.ref = self.head
-#         self.head = new_node
+    def add_at_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.ref:
+                current = current.ref
+            current.ref = new_node
 
 
-# LL1 = LinkedList()
-# # LL1.add_begin(10)
-# # LL1.add_begin(20)
-# # LL1.add_begin(30)
+    def add_begin(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
+
+
+
+
+def merge_lists(l1, l2):
+    merged = LinkedList()
+    cur1 = l1.head
+    cur2 = l2.head
+    while cur1 is not None and cur2 is not None:
+        if cur1.data < cur2.data:
+            merged.add_at_end(cur1.data)
+            cur1 = cur1.ref
+        else:
+            merged.add_at_end(cur2.data)
+            cur2 = cur2.ref
+    while cur1 is not None:
+        merged.add_at_end(cur1.data)
+        cur1 = cur1.ref
+    while cur2 is not None:
+        merged.add_at_end(cur2.data)
+        cur2 = cur2.ref
+    return merged
+
+LL1 = LinkedList()
+LL1.add_at_end(10)
+LL1.add_at_end(20)
+LL1.add_at_end(30)
 # LL1.print_LL()
+LL2 = LinkedList()
+LL2.add_at_end(15)
+LL2.add_at_end(28)
+LL2.add_at_end(29)
+
+
+merged_list = merge_lists(LL1, LL2)
+merged_list.print_LL()
+
+
 
 #############################################################
 
